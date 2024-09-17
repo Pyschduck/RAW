90c734b0db345f9b2bf7b64c98a16d95d7d9a4f9
Pyschduck <156922539+Pyschduck@users.noreply.github.com>
2024-09-17 11:35:01 +0530

    Update i2c_lib.c

--- i2c_lib.c
+++ i2c_lib.c
@@ -7,8 +7,8 @@
 static const char *MASTER_TAG = "i2c-master";
 static const char *SLAVE_TAG = "i2c-slave";
 
-int i2c_master_port = I2C_NUM_0;
-int i2c_slave_port = I2C_NUM_1;
+int i2c_master_port = 0;
+int i2c_slave_port = 1;
 
 static unsigned char SLAVE_ADDRESS = 0xA; 
 
@@ -28,33 +28,22 @@ esp_err_t master_init(int sda, int scl)
 
     esp_err_t err = i2c_param_config(i2c_master_port, &conf);
     if (err != ESP_OK) {
-        ESP_LOGE(MASTER_TAG, "I2C master param config failed: %s", esp_err_to_name(err));
         return err;
     }
-
-    err = i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
-    if (err != ESP_OK) {
-        ESP_LOGE(MASTER_TAG, "I2C master driver install failed: %s", esp_err_to_name(err));
-    }
-
-    return err;
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
 }
 
-esp_err_t master_write(void* data, size_t len)
+esp_err_t master_write(uint8_t message[], int len)
 {
-    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    ESP_LOGI(MASTER_TAG, "Sending Message = %s", message);  
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();    
     i2c_master_start(cmd);
-    i2c_master_write_byte(cmd, (SLAVE_ADDRESS << 1) | WRITE_BIT, ACK_CHECK_EN);
-    i2c_master_write(cmd, (uint8_t*)data, len, ACK_CHECK_EN);
+    i2c_master_write_byte(cmd, SLAVE_ADDRESS << 1 | WRITE_BIT, ACK_CHECK_EN);
+    i2c_master_write(cmd, message, len, ACK_CHECK_EN);
     i2c_master_stop(cmd);
-
+    
     esp_err_t ret = i2c_master_cmd_begin(i2c_master_port, cmd, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd);
-
-    if (ret != ESP_OK) {
-        ESP_LOGE(MASTER_TAG, "I2C master write failed: %s", esp_err_to_name(ret));
-    }
-
     return ret;
 }
 
@@ -106,13 +95,16 @@ esp_err_t slave_init(int sda, int scl)
 
     return err;
 }
-  uint8_t received_data[I2C_SLAVE_RX_BUF_LEN] = {0};
+
+uint8_t received_data[I2C_SLAVE_RX_BUF_LEN] = {0};
+
 void slave_read(void)
 {
-  
-    i2c_slave_read_buffer(i2c_slave_port, received_data, I2C_SLAVE_RX_BUF_LEN, 100 / portTICK_PERIOD_MS);
-    i2c_reset_rx_fifo(i2c_slave_port);
-
-    ESP_LOGI(SLAVE_TAG, "Data Received: %s", received_data);
+    size_t len = i2c_slave_read_buffer(i2c_slave_port, received_data, I2C_SLAVE_RX_BUF_LEN, 100 / portTICK_PERIOD_MS);
+    if (len > 0) {
+        ESP_LOGI(SLAVE_TAG, "Data Received: %s", received_data);
+    } else {
+        ESP_LOGI(SLAVE_TAG, "No data received");
+    }
     memset(received_data, 0, I2C_SLAVE_RX_BUF_LEN);
 }
