e1431a8ca521a11da1ac3625597ad4cdc12dbdc0
Pyschduck <156922539+Pyschduck@users.noreply.github.com>
2024-09-17 09:41:16 +0530

    Update i2c_lib.h

--- i2c_lib.h
+++ i2c_lib.h
@@ -8,6 +8,8 @@
 #define I2C_MASTER_FREQ_HZ 100000          /*!< I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE 0        /*!< freq master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE 0        /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_LEN 255
+#define I2C_MASTER_TX_BUF_len 255
 
 #define I2C_SLAVE_FREQ_HZ 100000          /*!< I2C slave clock frequency */
 #define I2C_SLAVE_TX_BUF_LEN 255          /*!< I2C slave transmit buffer length */
@@ -25,8 +27,9 @@ extern int i2c_master_port;
 extern int i2c_slave_port;
 
 esp_err_t master_init(int sda,int scl);
-esp_err_t master_write(uint8_t message[], int len,unsigned char slave_addr);
+esp_err_t master_write(void* data,size_t len, unsigned char slave_addr);
 esp_err_t slave_init(int sda,int scl);
 void slave_read(void);  
-
+esp_err_t master_read(void* data, size_t len, unsigned char slave_addr);
+esp_err_t slave_write(void* data, size_t len);
 #endif 
