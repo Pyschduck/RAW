3161e5e649d46aff9cb420826a2a3c3a59b24602
Pyschduck <156922539+Pyschduck@users.noreply.github.com>
2024-09-17 10:08:06 +0530

    Update i2c_slave_main.c

--- i2c_slave_main.c
+++ i2c_slave_main.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 #include <string.h>
 #include "esp_log.h"
-#include "driver/gpio.h"
 #include "i2c_lib.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-
-
-const char *TAG = "slave";
+const char *TAG = "i2c-slave";
 
 void app_main(void)
 {
-
-    ESP_ERROR_CHECK(slave_init(21,22));
+    ESP_ERROR_CHECK(slave_init(21, 22));
     ESP_LOGI(TAG, "I2C Slave initialized successfully");
 
-    while(1) {
+    while (1) {
         slave_read();
-        vTaskDelay(100 / portTICK_PERIOD_MS);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
