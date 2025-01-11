7c1665f2913e1bd22e5640f3bf62f92f3d4d928c
Pyschduck <156922539+Pyschduck@users.noreply.github.com>
2025-01-11 15:39:44 +0530

    Create Master_i2c.ino

---
+++ Master_i2c.ino
@@ -0,0 +1,56 @@
+#include <Wire.h>
+#include "PS4Controller.h"
+
+struct PS4Data {
+  int LStickX;
+  int LStickY;
+  int RStickX;
+  int RStickY;
+  bool Square;
+  bool Cross;
+  bool Circle;
+  bool Triangle;
+  bool touchpad;
+};
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();  // Initialize I2C as master
+  PS4.begin("78:2b:46:91:ef:af");  // Initialize PS4 controller with MAC address
+}
+
+void loop() {
+  if (PS4.isConnected()) {
+    PS4Data data;
+    data.LStickX = PS4.LStickX();
+    data.LStickY = PS4.LStickY();
+    data.RStickX = PS4.RStickX();
+    data.RStickY = PS4.RStickY();
+    data.Square = PS4.Square();
+    data.Cross = PS4.Cross();
+    data.Circle = PS4.Circle();
+    data.Triangle = PS4.Triangle();
+    data.touchpad = PS4.Touchpad();
+
+    Wire.beginTransmission(0x01);  // Begin transmission to slave with address 0x01
+    Wire.write((uint8_t*)&data, sizeof(data));  // Send PS4 data
+    byte checksum = calculateChecksum((uint8_t*)&data, sizeof(data));  // Calculate checksum
+    Wire.write(checksum);  // Send checksum
+
+    byte error = Wire.endTransmission();  // End transmission
+
+    if (error == 0) {
+      Serial.println("Data sent successfully to Slave 0x01");
+    } else {
+      Serial.println("Error sending to Slave 0x01");
+    }
+  }
+}
+
+byte calculateChecksum(uint8_t *data, size_t length) {
+  byte sum = 0;
+  for (size_t i = 0; i < length; i++) {
+    sum += data[i];
+  }
+  return sum;
+}
